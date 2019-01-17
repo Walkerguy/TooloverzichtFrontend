5cbb0e7c9dbd44685f2a686afd5e7375dcb31c9e
Bram <shep.ukn@gmail.com>
2019-01-17 16:33:24 +0100

    Camelcase

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 const path = require('path');
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tool-frontend'));
+app.use(express.static(__dirname + '/dist/toolFrontend'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/tool-frontend/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/toolFrontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
