ce63560a6e0e5fab806b30b5f36f7cc17af9ef90
Bram <shep.ukn@gmail.com>
2019-01-17 13:19:56 +0100

    Re-added project name

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/tool-frontend/'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/tool-frontend/index.html'));
 
 });
 
