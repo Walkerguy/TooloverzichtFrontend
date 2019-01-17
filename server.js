c2fe805c0ad26b62fbbe425163d9b4cbc71bd4a4
Bram <shep.ukn@gmail.com>
2019-01-17 13:02:34 +0100

    Another try

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const http = require('http');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tool-frontend'));
+app.use(express.static(__dirname + '/dist/'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/tool-frontend/index.html'));
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+
 });
 
 // Start the app by listening on the default Heroku port
