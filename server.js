2c75b67b85a16c5ab572b044a48c55a04eeed8ab
Bram <shep.ukn@gmail.com>
2019-01-17 13:34:35 +0100

    added --aot

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tool-frontend/'));
+app.use(express.static('./dist/tool-frontend'));
 
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/tool-frontend/index.html'));
+app.get('/*', function(req,res) {
 
+res.sendFile(path.join(__dirname,'/dist/tool-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
